327c25c9481fd3712a6d93c8bf275dc4e0e5b8de
Kieran Dunch <dunch.kieran@gmail.com>
2022-03-02 12:01:45 -0500

    added index view and method

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,14 +1,16 @@
 class RecipesController < ApplicationController
+  def index
+    @recipes = Recipe.all
+  end
+
   def new
     @recipe = Recipe.new
   end
 
   def create
-    #  create globaL RECIPE
     @recipe = Recipe.new(recipe_params)
     @recipe.user = current_user
     if @recipe.save
-      # create user recipe
       user_recipe = UserRecipe.create(recipe: @recipe, user: current_user)
       redirect_to my_cookbook_path(user_recipe.user), notice: "recipe was created"
     else

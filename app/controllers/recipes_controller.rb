8ce147266d43e4a49f874b4dcbf5a23dbf2e45f6
Kieran Dunch <dunch.kieran@gmail.com>
2022-03-02 11:46:00 -0500

    finished all create recipe logic basics, including form partial

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,24 @@
 class RecipesController < ApplicationController
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    #  create globaL RECIPE
+    @recipe = Recipe.new(recipe_params)
+    @recipe.user = current_user
+    if @recipe.save
+      # create user recipe
+      user_recipe = UserRecipe.create(recipe: @recipe, user: current_user)
+      redirect_to my_cookbook_path(user_recipe.user), notice: "recipe was created"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:title, :description, :prep_time, :instructions, :ingredients, :category)
+  end
 end

7f5c37ce57c8a1015c1523f0f38ff335d3eb5d30
Kieran Dunch <dunch.kieran@gmail.com>
2022-03-02 16:52:21 -0500

    Here is the improved seed with associations

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,18 @@ User.destroy_all
 Recipe.destroy_all
 UserRecipe.destroy_all
 
+puts "creating admin account"
+user = User.new(
+  email: "admin@test.com",
+  password: 123456,
+  first_name: "Kieran",
+  last_name: "Dunch",
+  location: Faker::Address.city
+)
+puts user.email
+puts user.password
+user.save!
+
 puts "Creating users"
 
 15.times do
@@ -15,8 +27,8 @@ puts "Creating users"
     location: Faker::Address.city
   )
 
-  user.save!
   puts user.first_name
+  user.save!
 end
 
 puts "Creating recipes"
@@ -27,17 +39,21 @@ puts "Creating recipes"
     ingredients: "#{Faker::Food.ingredient},#{Faker::Food.measurement}, #{Faker::Food.ingredient}, #{Faker::Food.measurement}",
     prep_time: "#{Faker::Number.between(from: 1, to: 10)} hours",
     instructions: "Cook the #{Faker::Food.ingredient} on #{["high", "low", "medium"].sample} for 10 minutes. Add the #{Faker::Food.spice}. Bake for #{Faker::Number.between(from: 1, to: 10)} hours. Then add #{Faker::Food.measurement} of #{Faker::Food.spice}. Prepare the #{Faker::Food.fruits} for dessert",
-    category: Faker::Food.ethnic_category
+    category: Faker::Food.ethnic_category,
   )
   image_file = scraping(recipe)
   recipe.photo.attach(io: image_file, filename: recipe.title, content_type: 'image/png')
   user = User.all.sample
   recipe.user = user
+  user_recipe = UserRecipe.new(
+    user: user, recipe: recipe
+  )
   recipe.save!
+  user_recipe.save!
   puts recipe.title
 end
 
-puts "Creating user_recipes"
+puts "creating user recipes..."
 
 50.times do
   user = User.all.to_a.sample

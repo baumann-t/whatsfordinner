40602d1010487463d12a9e4d994018a41fdfbdaf
Kieran Dunch <dunch.kieran@gmail.com>
2022-03-02 17:06:08 -0500

    added serving size to recipes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_02_220107) do
+ActiveRecord::Schema.define(version: 2022_03_02_220327) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,6 +72,7 @@ ActiveRecord::Schema.define(version: 2022_03_02_220107) do
     t.datetime "updated_at", precision: 6, null: false
     t.text "ingredients"
     t.string "category"
+    t.string "serving_size"
     t.index ["user_id"], name: "index_recipes_on_user_id"
   end
 

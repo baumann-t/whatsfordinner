f0154b48760ee84710c3dd4fc852fbe981419472
Kieran Dunch <dunch.kieran@gmail.com>
2022-03-03 14:44:55 -0500

    added js methods for controller

--- app/javascript/controllers/form_controller.js
+++ app/javascript/controllers/form_controller.js
@@ -1,16 +1,45 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = ["desc"]
+  static targets = ["desc","prep","instructions","ingredients","category","photo"]
 
   connect() {
     console.log("connected")
   }
 
-  prevent (event) {
-    console.log(this.descriptionTarget)
+  preventDesc (event) {
+    console.log(this.descTarget)
     event.preventDefault()
-    this.descriptionTarget.scrollIntoView()
+    this.descTarget.scrollIntoView()
+  }
 
+  preventPrep (event) {
+    console.log(this.prepTarget)
+    event.preventDefault()
+    this.prepTarget.scrollIntoView()
+  }
+
+  preventInstructions (event) {
+    console.log(this.instructionsTarget)
+    event.preventDefault()
+    this.instructionsTarget.scrollIntoView()
+  }
+
+  preventIngredients (event) {
+    console.log(this.ingredientsTarget)
+    event.preventDefault()
+    this.ingredientsTarget.scrollIntoView()
+  }
+
+  preventCategory (event) {
+    console.log(this.categoryTarget)
+    event.preventDefault()
+    this.categoryTarget.scrollIntoView()
+  }
+
+  preventPhoto (event) {
+    console.log(this.photoTarget)
+    event.preventDefault()
+    this.photoTarget.scrollIntoView()
   }
 }

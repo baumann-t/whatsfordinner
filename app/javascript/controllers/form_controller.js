9c7020cbef5174b349363047ecd0c5c38b16b23e
Kieran Dunch <dunch.kieran@gmail.com>
2022-03-06 13:51:55 -0500

    finished back button implementation and fred-refactor

--- app/javascript/controllers/form_controller.js
+++ app/javascript/controllers/form_controller.js
@@ -1,57 +1,25 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = ["desc","prep","cooking", "servings","instructions","ingredients","category","photo"]
+  static targets = ["formSpace"]
 
   connect() {
     console.log("connected")
   }
 
-  preventDesc (event) {
-    console.log(this.descTarget)
-    event.preventDefault()
-    this.descTarget.scrollIntoView()
-  }
+    goToNextFormSpace (event) {
+      event.preventDefault()
+      const currentFormSpacePosition = event.target.getAttribute("data-position")
+      const nextFormSpacePosition = parseInt(currentFormSpacePosition) + 1
+      const nextFormSpace = this.formSpaceTargets[nextFormSpacePosition]
+      nextFormSpace.scrollIntoView()
+    }
 
-  preventPrep (event) {
-    console.log(this.prepTarget)
-    event.preventDefault()
-    this.prepTarget.scrollIntoView()
-  }
-
-  preventCooking (event) {
-    console.log(this.cookingTarget)
-    event.preventDefault()
-    this.cookingTarget.scrollIntoView()
-  }
-
-  preventServings (event) {
-    console.log(this.servingsTarget)
-    event.preventDefault()
-    this.servingsTarget.scrollIntoView()
-  }
-
-  preventIngredients (event) {
-    console.log(this.ingredientsTarget)
-    event.preventDefault()
-    this.ingredientsTarget.scrollIntoView()
-  }
-
-  preventInstructions (event) {
-    console.log(this.instructionsTarget)
-    event.preventDefault()
-    this.instructionsTarget.scrollIntoView()
-  }
-
-  preventCategory (event) {
-    console.log(this.categoryTarget)
-    event.preventDefault()
-    this.categoryTarget.scrollIntoView()
-  }
-
-  preventPhoto (event) {
-    console.log(this.photoTarget)
-    event.preventDefault()
-    this.photoTarget.scrollIntoView()
-  }
+    goToLastFormSpace (event) {
+      event.preventDefault()
+      const currentFormSpacePosition = event.target.getAttribute("data-position")
+      const nextFormSpacePosition = parseInt(currentFormSpacePosition) - 1
+      const nextFormSpace = this.formSpaceTargets[nextFormSpacePosition]
+      nextFormSpace.scrollIntoView()
+    }
 }

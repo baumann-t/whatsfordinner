a39b7f3f1450bb6a737c051851fb7907feb3383e
Thomas Baumann <tbthomas.baumann@gmail.com>
2022-03-06 14:50:44 -0500

    implemented a dynamic like/unlike method for recipes, including visual icons

--- app/javascript/controllers/upvotes_controller.js
+++ app/javascript/controllers/upvotes_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "stimulus"
 import { csrfToken } from "@rails/ujs"
 
 export default class extends Controller {
-  static targets = ["submit", "count"]
+  static targets = ["submit", "count", "remove"]
   static values = {recipe: Number}
 
   connect() {
@@ -14,8 +14,40 @@ export default class extends Controller {
     fetch(url, {headers: { "Accept": "application/json", "X-CSRF-Token": csrfToken() }})
     .then(response => response.json())
     .then((data) => {
-      this.countTarget.innerHTML = `${data["upvote_count"]} upvotes`
-      this.submitTarget.style.visibility = 'hidden'
+      let string = ""
+      if (data["upvote_count"] === 0) {
+        string = `${data["upvote_count"]} like`
+      } else if (data["upvote_count"] === 1){
+        string = `${data["upvote_count"]} like`
+      } else {
+        string = `${data["upvote_count"]} likes`
+      }
+      this.countTarget.innerHTML = string
+      this.submitTarget.style.display = "none";
+      this.removeTarget.style.display = "unset";
+    })
+  }
+
+  unvote(event) {
+    event.preventDefault()
+    const url = `/recipes/${this.recipeValue}/upvotes`
+    fetch(url, {
+      method: "DELETE",
+      headers: { "Accept": "application/json", "X-CSRF-Token": csrfToken() },
+    })
+    .then(response => response.json())
+    .then((data) => {
+      let string = ""
+      if (data["upvote_count"] === 0) {
+        string = `${data["upvote_count"]} like`
+      } else if (data["upvote_count"] === 1){
+        string = `${data["upvote_count"]} like`
+      } else {
+        string = `${data["upvote_count"]} likes`
+      }
+      this.countTarget.innerHTML = string
+      this.submitTarget.style.display = "unset";
+      this.removeTarget.style.display = "none";
     })
   }
 }
--- app/models/upvote.rb
+++ app/models/upvote.rb
@@ -1,4 +1,6 @@
 class Upvote < ApplicationRecord
   belongs_to :user
   belongs_to :recipe
+
+  validates :user_id, uniqueness: { scope: :recipe_id }
 end

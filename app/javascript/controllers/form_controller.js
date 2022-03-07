99d01668dfce74632d99110fc0647b44d825c06c
Thomas Baumann <61998152+thomasbaumann-mtl@users.noreply.github.com>
2022-03-07 17:59:28 -0500

    Revert "Create recipe refinement"

--- app/javascript/controllers/form_controller.js
+++ app/javascript/controllers/form_controller.js
@@ -1,7 +1,5 @@
 import { Controller } from "stimulus"
 
-let currentCategoryPosition = 1
-
 export default class extends Controller {
   static targets = ["formSpace"]
 
@@ -9,20 +7,19 @@ export default class extends Controller {
     console.log("connected")
   }
 
-  goToNextFormSpace (event) {
-    event.preventDefault()
-    const currentFormSpacePosition = event.target.getAttribute("data-position")
-    const nextFormSpacePosition = parseInt(currentFormSpacePosition) + 1
-    const nextFormSpace = this.formSpaceTargets[nextFormSpacePosition]
-    nextFormSpace.scrollIntoView()
-  }
+    goToNextFormSpace (event) {
+      event.preventDefault()
+      const currentFormSpacePosition = event.target.getAttribute("data-position")
+      const nextFormSpacePosition = parseInt(currentFormSpacePosition) + 1
+      const nextFormSpace = this.formSpaceTargets[nextFormSpacePosition]
+      nextFormSpace.scrollIntoView()
+    }
 
-  goToLastFormSpace (event) {
-    event.preventDefault()
-    const currentFormSpacePosition = event.target.getAttribute("data-position")
-    const nextFormSpacePosition = parseInt(currentFormSpacePosition) - 1
-    const nextFormSpace = this.formSpaceTargets[nextFormSpacePosition]
-    nextFormSpace.scrollIntoView()
-  }
-
+    goToLastFormSpace (event) {
+      event.preventDefault()
+      const currentFormSpacePosition = event.target.getAttribute("data-position")
+      const nextFormSpacePosition = parseInt(currentFormSpacePosition) - 1
+      const nextFormSpace = this.formSpaceTargets[nextFormSpacePosition]
+      nextFormSpace.scrollIntoView()
+    }
 }
--- db/migrate/20220307165800_revome_ingredients.rb
+++
@@ -1,5 +0,0 @@
-class RevomeIngredients < ActiveRecord::Migration[6.1]
-  def change
-    remove_column :recipes, :ingredients
-  end
-end

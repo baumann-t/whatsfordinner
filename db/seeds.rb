1b0862a5c70be7423cdaf31748f4eba993033687
Huang Shewei <huangshewei@gmail.com>
2022-03-07 15:26:57 -0500

    Seed is generating always the same results

--- db/seeds.rb
+++ db/seeds.rb
@@ -52,8 +52,36 @@ puts lea.first_name
 lea.save!
 # ----- CREATE USER END -----
 
-# ----- CREATE RECIPE START -----
 
+# ----- Create Recipe end -----
+# ----- Creating Users
+
+puts "Creating users"
+
+10.times do
+  user = User.new(
+    email: Faker::Internet.unique.email,
+    password: 123456,
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    location: Faker::Address.city
+  )
+
+  puts user.first_name
+  user.save!
+end
+
+huang = User.new(
+  email: "huang@whats4dinner.app",
+  password: 123456,
+  first_name: "Huang",
+  last_name: "Shewei",
+  location: "Montreal"
+)
+
+
+
+# ----- CREATE RECIPE START -----
 shrimp_pasta = Recipe.new(
   title: "Shrimp Pasta",
   description: "A simple yet delicious pasta with shrimps",
@@ -176,10 +204,6 @@ user_recipe.save!
 puts egg_sandwich.title
 
 # need to associate recipe id with my cookbook and mark as cooked
-# seb_us = UserRecipe.new(
-#   user: seb, recipe: egg_sandwich, cooked: true, date_cooked: Date.yesterday
-# )
-# seb_us.save!
 
 comment = Comment.new(content: "This is the worst crap I've ever tasted!")
 comment.user = User.first
@@ -191,114 +215,87 @@ comment.save!
 tomato_pasta = Recipe.new(
   title: "Tomato Pasta",
   description: "This dish will always leave you satisfied! Trust me ;)",
-  ingredients: "#{Faker::Food.ingredient} (#{Faker::Food.measurement}), #{Faker::Food.ingredient} (#{Faker::Food.measurement})",
-  prep_time: "#{Faker::Number.between(from: 1, to: 10)} hours",
-  instructions: "Cook the #{Faker::Food.ingredient} on #{['high', 'low', 'medium'].sample} for 10 minutes. Add the #{Faker::Food.spice}. Bake for #{Faker::Number.between(from: 1, to: 10)} hours. Then add #{Faker::Food.measurement} of #{Faker::Food.spice}. Prepare the #{Faker::Food.fruits} for dessert",
+  ingredients: "500g penne, 2 tablespoon of olive oil, 1 small onion, 2 garlic cloves, salt to taste,
+                ground black pepper to taste, 1 can crushed tomatoes, 1/2 cup of heavy cream, 1/2 cup of basil leaves,
+                grated parmesan to taste",
+  prep_time: "35 minutes",
+  instructions: "Heat olive oil in a large, high-sided sauté pan over medium heat until shimmering.
+                Add the onion and sauté until softened and translucent, 3 to 4 minutes.
+                Add the garlic and red pepper flakes, if using, and sauté until fragrant, 30 seconds to 1 minute.
+                Carefully pour in the crushed tomatoes. Add the salt and pepper and stir to combine.
+                Bring to a simmer and cook, uncovered, stirring occasionally, for 10 minutes.
+                Reduce heat to low and stir in the cream. Taste and season with additional salt and pepper as needed.
+                Meanwhile, bring a large pot of salted water to a boil. Add the pasta and cook al dente, about
+                10 minutes or according to package instructions. Drain the pasta and add it to the sauce. Toss gently to
+                combine. Add the basil, toss once more, and serve immediately with grated cheese.",
   category: "Italian",
-  serving_size: "#{Faker::Number.between(from: 2, to: 10)} servings",
+  serving_size: "4 to 6 servings",
   upvotes_tracker: 11
 )
-image_file = scraping("Tomato Pasta")
+image_file = URI.open('https://cdn.apartmenttherapy.info/image/upload/f_auto,q_auto:eco,c_fill,g_center,w_1460,h_1825/k%2Farchive%2F0eb929bc2c007ebf6009316a327b1e37587045a4')
 tomato_pasta.photo.attach(io: image_file, filename: tomato_pasta.title, content_type: 'image/png')
 tomato_pasta.user = kieran
+tomato_pasta.save!
 user_recipe = UserRecipe.new(
   user: seb, recipe: tomato_pasta
 )
+user_recipe.save!
 
 comment = Comment.new(content: "This was actually pretty good!!!!")
-comment.user = User.first
+comment.user = seb
 comment.recipe = tomato_pasta
 comment.user_recipe = user_recipe
 comment.save!
 
-tomato_pasta.save!
-user_recipe.save!
 puts tomato_pasta.title
 
-puts "Creating users"
+# Another recipe
 
-20.times do
-  user = User.new(
-    email: Faker::Internet.unique.email,
-    password: 123456,
-    first_name: Faker::Name.first_name,
-    last_name: Faker::Name.last_name,
-    location: Faker::Address.city
-  )
+garlic_butter_steak = Recipe.new(
+  title: "Garlic butter steak",
+  description: "Delicious butter steak perfect for dinner.",
+  ingredients: "1 ribeye steak, salt to taste, ground black pepper to taste,
+                3 tablespoon canola oil, 3 tablespoon of butter, 3 cloves of garlic,
+                2 sprigs fresh thyme",
+  prep_time: "1 hour",
+  instructions: "Preheat oven to 200°F (95°C). Generously season all sides of the steak with salt and pepper.
+                Transfer to a wire rack on top of a baking sheet, then bake for about 45 minutes to an hour until the
+                internal temperature reads about 125°F (51° C) for medium-rare. Adjust the bake time based on if you
+                like your steak more rare or more well-done. Heat the canola oil in a pan over high heat until smoking.
+                Do not use olive oil, as its smoke point is significantly lower than that of canola oil and will smoke
+                before reaching the desired cooking temperature. Sear the steak for 30 seconds on the first side,
+                then flip. Add the butter, garlic, rosemary, and thyme and swirl around the pan. Transfer the garlic
+                and herbs on top of the steak and baste the steak with the butter using a large spoon. Baste for about
+                30 seconds, then flip and baste the other side for about 15 seconds. Turn the steak on its side and cook
+                to render off any excess fat. Rest the steak on a cutting board or wire rack for about 10 minutes.
+                Slicing the steak before the resting period has finished will result in a lot of the juices leaking out,
+                which may not be desirable.
+                Slice the steak into ½ -inch (1 cm) strips, then fan out the slices and serve.",
+  category: "American",
+  serving_size: "2",
+  upvotes_tracker: 4
+)
+image_file = URI.open('https://www.eatwell101.com/wp-content/uploads/2020/10/Garlic-Butter-Steak-recipe-roasted-in-Oven.jpg')
+garlic_butter_steak.photo.attach(io: image_file, filename: garlic_butter_steak, content_type: 'image/png')
+garlic_butter_steak.user = huang
+garlic_butter_steak.save!
 
-  puts user.first_name
-  user.save!
-end
+user_recipe = UserRecipe.new(
+  user: huang, recipe: garlic_butter_steak
+)
+user_recipe.save!
+puts garlic_butter_steak.title
 
-puts "Creating recipes"
-# @foods = ["Pho", "Cauliflower Penne", "Fettuccine Alfredo", "Mushroom Risotto", "Scotch Eggs", "Caprese Salad",
-#           "Chicken Milanese", "Lasagne", "Katsu Curry", "Barbecue Ribs", "Chicken Fajitas", "Arepas", "Chili con Carne",
-#           "Tuna Sashimi", "Fish and Chips", "Pork Sausage Roll", "Hummus", "Poutine", "California Maki",
-#           "Linguine with Clams", "French Fries with Sausages", "Bruschette with Tomato", "Pork Belly Buns",
-#           "French Toast", "Souvlaki", "Pierogi", "Pasta and Beans", "Salmon Nigiri", "Bunny Chow", "Peking Duck",
-#           "Pizza", "Kebab", "Cheeseburger", "Ebiten maki"]
+# need to associate recipe id with my cookbook and mark as cooked
 
-@foods = ["Pho", "Cauliflower Penne", "Fettuccine Alfredo", "Mushroom Risotto", "Scotch Eggs", "Caprese Salad"]
+comment = Comment.new(content: "This is so delicious! I cook it every day")
+comment.user = lea
+comment.recipe = garlic_butter_steak
+comment.user_recipe = user_recipe
+comment.save!
 
-@foods.each do |food|
-  recipe = Recipe.new(
-    title: food,
-    description: Faker::Food.description,
-    ingredients: "#{Faker::Food.ingredient} (#{Faker::Food.measurement}), #{Faker::Food.ingredient} (#{Faker::Food.measurement})",
-    prep_time: "#{Faker::Number.between(from: 1, to: 10)} hours",
-    instructions: "Cook the #{Faker::Food.ingredient} on #{["high", "low", "medium"].sample} for 10 minutes. Add the #{Faker::Food.spice}. Bake for #{Faker::Number.between(from: 1, to: 10)} hours. Then add #{Faker::Food.measurement} of #{Faker::Food.spice}. Prepare the #{Faker::Food.fruits} for dessert",
-    category: Faker::Food.ethnic_category,
-    serving_size: "#{Faker::Number.between(from: 2, to: 10)} servings"
-  )
-
-  image_file = scraping(food)
-  recipe.photo.attach(io: image_file, filename: recipe.title, content_type: 'image/png')
-  user = User.all.sample
-  recipe.user = user
-  user_recipe = UserRecipe.new(
-    user: user, recipe: recipe
-  )
-  recipe.save!
-  user_recipe.save!
-  puts recipe.title
-end
-
-puts "creating user recipes..."
-
-10.times do
-  user = User.all.sample
-  recipe = Recipe.all.to_a.sample
-  user_recipe = UserRecipe.new(
-    cooked: [false, true].sample,
-    date_cooked: Faker::Date.in_date_period(year: 2018, month: 2),
-    wishlisted: [false, true].sample,
-    user_comment: ["Wow so good!", "Still have to try this one!", "I eat this at least once a week",
-                   "Easy recipe for week night dinner"].sample
-  )
-  user_recipe.user = user
-  user_recipe.recipe = recipe
-  user_recipe.save!
-end
-
-# puts "creating upvotes and comments"
-# 20.times do
-#   user = User.all.sample
-#   recipe = Recipe.all.sample
-#   content = ["So nice!", "I'll try that next week!", "Added to my cookbook right away!", "Are you sure we need that much sugar?", "Can I replace the wine with beef stock?", "You should go to Top Chef!"].sample
-
-#   comment = Comment.new(content: content)
-#   comment.user = user
-#   comment.recipe = recipe
-#   comment.user_recipe = user_recipe
-#   comment.save!
-
-#   upvote = Upvote.new
-#   upvote.user = user
-#   upvote.recipe = recipe
-#   upvote.save!
-# end
 puts "creating upvotes and comments"
-20.times do
+10.times do
   user = User.all.sample
   recipe = Recipe.all.sample
 
@@ -318,5 +315,4 @@ puts "creating upvotes and comments"
     upvote.recipe = recipe
     upvote.save!
   end
-
 end

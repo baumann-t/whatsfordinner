e09a1d1891e7020d42b0fbbd88ba470391019cc0
Thomas Baumann <tbthomas.baumann@gmail.com>
2022-03-08 09:54:03 -0500

    fixed bugs

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -41,6 +41,9 @@ class RecipesController < ApplicationController
   def create
     @recipe = Recipe.new(recipe_params)
     @recipe.user = current_user
+    @recipe.ingredients = params[:recipe][:ingredients].values
+    @recipe.instructions = params[:recipe][:instructions].values
+    @recipe.categories = params[:recipe][:categories].values
     if @recipe.save
       user_recipe = UserRecipe.create(recipe: @recipe, user: current_user)
       redirect_to my_cookbook_path(user_recipe.user), notice: "Your recipe was created!"
@@ -52,6 +55,7 @@ class RecipesController < ApplicationController
   private
 
   def recipe_params
-    params.require(:recipe).permit(:title, :description, :prep_time, :instructions, :ingredients, :category, :photo, :serving_size)
+    params.require(:recipe).permit(:title, :description, :prep_time, :cooking_time, :photo,
+                                   :serving_size, instructions: [], ingredients: [], categories: [])
   end
 end

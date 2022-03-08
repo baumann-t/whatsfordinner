55bc8d6398e3af293195a3b24efed43d46d4c4d1
Kieran Dunch <dunch.kieran@gmail.com>
2022-03-08 17:00:48 -0500

    fixed enter button on js controller

--- app/javascript/controllers/form_controller.js
+++ app/javascript/controllers/form_controller.js
@@ -5,21 +5,31 @@ export default class extends Controller {
 
   connect() {
     console.log("connected")
+    window.addEventListener("keyup", this.preventEnter)
+  }
+  disconnect() {
+    window.removeEventListener("keyup", this.preventEnter)
   }
 
-    goToNextFormSpace (event) {
-      event.preventDefault()
-      const currentFormSpacePosition = event.target.getAttribute("data-position")
-      const nextFormSpacePosition = parseInt(currentFormSpacePosition) + 1
-      const nextFormSpace = this.formSpaceTargets[nextFormSpacePosition]
-      nextFormSpace.scrollIntoView()
-    }
+  goToNextFormSpace (event) {
+    event.preventDefault()
+    const currentFormSpacePosition = event.target.getAttribute("data-position")
+    const nextFormSpacePosition = parseInt(currentFormSpacePosition) + 1
+    const nextFormSpace = this.formSpaceTargets[nextFormSpacePosition]
+    nextFormSpace.scrollIntoView()
+  }
 
-    goToLastFormSpace (event) {
-      event.preventDefault()
-      const currentFormSpacePosition = event.target.getAttribute("data-position")
-      const nextFormSpacePosition = parseInt(currentFormSpacePosition) - 1
-      const nextFormSpace = this.formSpaceTargets[nextFormSpacePosition]
-      nextFormSpace.scrollIntoView()
-    }
+  goToLastFormSpace (event) {
+    event.preventDefault()
+    const currentFormSpacePosition = event.target.getAttribute("data-position")
+    const nextFormSpacePosition = parseInt(currentFormSpacePosition) - 1
+    const nextFormSpace = this.formSpaceTargets[nextFormSpacePosition]
+    nextFormSpace.scrollIntoView()
+  }
+
+  preventEnter(event) {
+    console.log(event.key)
+    if (event.key !== "Enter") return
+    event.preventDefault()
+  }
 }

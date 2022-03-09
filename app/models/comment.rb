8cb43412ee67aad54f5bcb041ce9a948360685f9
Thomas Baumann <tbthomas.baumann@gmail.com>
2022-03-09 13:47:44 -0500

    added creation date on comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,8 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :recipe
   belongs_to :user_recipe
+
+  def created_at_formated
+    self.created_at.strftime('%b %e, %l:%M %p')
+  end
 end

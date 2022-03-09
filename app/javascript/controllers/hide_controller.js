ca2a25e47d42be784c968be1604819c5db3fb33a
Thomas Baumann <tbthomas.baumann@gmail.com>
2022-03-09 10:28:12 -0500

    created a dynamic value for the number of comments

--- app/javascript/controllers/hide_controller.js
+++ app/javascript/controllers/hide_controller.js
@@ -1,18 +1,27 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = ["comments", "view", "hide"]
+  static targets = ["comments", "view", "hide", "text", "list"]
   static values = {recipe: Number, userecipe: Number}
 
   show() {
     this.commentsTarget.style.display = "unset";
     this.viewTarget.style.display = "none";
     this.hideTarget.style.display = "unset";
+
   }
 
   hide() {
     this.commentsTarget.style.display = "none";
     this.viewTarget.style.display = "unset";
     this.hideTarget.style.display = "none";
+    const commentNum = document.querySelectorAll("#comment-list-li").length
+    if (commentNum === 1){
+      this.textTarget.innerHTML = "VIEW 1 COMMENT"
+    } else if (commentNum === 0){
+      this.textTarget.innerHTML = "ADD A COMMENT"
+    } else {
+      this.textTarget.innerHTML = `VIEW ${commentNum} COMMENTS`
+    }
   }
 }
